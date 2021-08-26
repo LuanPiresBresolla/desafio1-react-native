a37068a3ce660bca4c362fdd6097a1d688192e98
Luan Bresolla <luanpires34@gmail.com>
2021-08-25 21:08:10 -0300

    Finalizando teste

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,15 +9,34 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    if (newTaskTitle.length > 0) {
+      const taks = {
+        id: new Date().getTime(),
+        title: newTaskTitle,
+        done: false,
+      }
+  
+      setTasks(state => [...state, taks]);
+    }
   }
 
   function handleToggleTaskDone(id: number) {
     //TODO - toggle task done if exists
+    setTasks(state => state.map(item => {
+      if (item.id === id) {
+        return {
+          ...item,
+          done: !item.done,
+        };
+      }
+
+      return item;
+    }));
   }
 
   function handleRemoveTask(id: number) {
     //TODO - remove task from state
+    setTasks(state => state.filter(item => item.id !== id));
   }
 
   return (
